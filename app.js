9c5c94ecdfbaa65e12db34b9e2e55bcc1182e4f5
ErikStadel <helmstroek@gmail.com>
2025-07-18 10:48:52 +0000

    new app js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(serveStatic(__dirname, { index: ['index.html'] })); // Serviert index.html, falls vorhanden
+app.use(serveStatic(__dirname, { index: ['index.html'] })); // Serviert index.html
 
 const uri = process.env.MONGODB_URI;
 const dbName = process.env.DB_NAME || 'wineDB';
