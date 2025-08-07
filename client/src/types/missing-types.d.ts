fc7f7a1a84085c01bf9d7e78524cbc88b8068f91
ErikStadel <helmstroek@gmail.com>
2025-08-07 12:51:52 +0000

    Clean Up

---
+++ client/src/types/missing-types.d.ts
@@ -0,0 +1,40 @@
+declare module 'babel__core' {
+  export {};
+}
+
+declare module 'babel__generator' {
+  export {};
+}
+
+declare module 'babel__template' {
+  export {};
+}
+
+declare module 'babel__traverse' {
+  export {};
+}
+
+declare module 'bonjour' {
+  export {};
+}
+
+declare module 'eslint' {
+  export {};
+}
+
+declare module 'estree' {
+  export {};
+}
+
+declare module 'prettier' {
+  export {};
+}
+
+declare module 'react' {
+  export * from 'react'; // falls du wirklich React nutzt
+  export default any;
+}
+
+declare module 'trusted-types' {
+  export {};
+}
