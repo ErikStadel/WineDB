51ed900fd81718c8a6f754f9b2e181752be40575
ErikStadel <helmstroek@gmail.com>
2025-12-29 13:33:36 +0100

    Add synonyms to autocomplete configuration

--- server/js/app.js
+++ server/js/app.js
@@ -287,7 +287,7 @@ app.get('/wines/search', async (req, res) => {
             autocomplete: {
               query: q.trim(),
               path: 'rebsorte_autocomplete',
-              fuzzy: { maxEdits: 2 }
+              synonyms: 'Rebsorten'
             },
           },
           {
