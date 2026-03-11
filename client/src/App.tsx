a46cacfd2b1b6bd281fa71c656555e6fd9337876
ErikStadel <helmstroek@gmail.com>
2026-03-11 09:52:12 +0000

    Refinded BG and Buttons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -165,7 +165,7 @@ const App: React.FC = () => {
 
         <footer className="footer">
           <p>❤ We Love Wein ❤</p>
-          <p className="version-text">v 2.6.1</p>
+          <p className="version-text">v 2.6.2</p>
         </footer>
       </div>
     </>
