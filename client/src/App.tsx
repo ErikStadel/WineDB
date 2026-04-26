8ca9eef662773e471058a12ff67e671ecb1fc175
ErikStadel <helmstroek@gmail.com>
2026-04-26 09:22:23 +0000

    Version 2.8

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -165,7 +165,7 @@ const App: React.FC = () => {
 
         <footer className="footer">
           <p>❤ We Love Wein ❤</p>
-          <p className="version-text">v 2.7</p>
+          <p className="version-text">v 2.8</p>
         </footer>
       </div>
     </>
